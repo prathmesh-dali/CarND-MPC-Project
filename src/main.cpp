ea5cc00a851d7da760fa82772cb76f6b60e85134
Prathmesh Dali <prathmesh.dali@ad.infosys.com>
2018-05-22 12:03:04 +0530

    to be worked on

--- src/main.cpp
+++ src/main.cpp
@@ -98,8 +98,37 @@ int main() {
           * Both are in between [-1, 1].
           *
           */
+
+         Eigen::VectorXd transformed_x(ptsx.size());
+         Eigen::VectorXd transformed_y(ptsy.size());
+
+         for(int i = 0; i < ptsy.size(); i++){
+           double d_x = ptsx[i]-px;
+           double d_y = ptsy[i]-py;
+
+           transformed_x[i] = (d_x * cos(-psi) - d_y * sin(-psi));
+           transformed_y[i] = (d_x * sin(-psi) + d_y * cos(-psi));
+         }
+
+         auto coeffs = polyfit(transformed_x, transformed_y, 3);
+
+         double cte = polyeval(coeffs, 0);
+
+         double epsi = -atan(coeffs[1]);
+
+         Eigen::VectorXd state(6);
+
+         state<<0, 0, 0, 0, cte, epsi;
+
+         auto vars = mpc.Solve(state, coeffs);
+         
           double steer_value;
           double throttle_value;
+
+          const double Lf = 2.67;
+
+          steer_value = -vars[0]/(deg2rad(25)*Lf);
+          throttle_value = vars[1];
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
@@ -114,6 +143,15 @@ int main() {
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
 
+          for(int i = 2; i< vars.size(); i++){
+            if(i%2==0){
+              mpc_x_vals.push_back(vars[i]);
+            }
+            else{
+              mpc_y_vals.push_back(vars[i]);  
+            }
+          }
+
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
 
@@ -123,6 +161,13 @@ int main() {
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
+
+          double poly_inc = 2.5;
+          int num_points = 25;
+          for(int i = 1; i<num_points; i++){
+            next_x_vals.push_back(poly_inc*i);
+            next_y_vals.push_back(polyeval(coeffs, poly_inc*i));
+          }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
